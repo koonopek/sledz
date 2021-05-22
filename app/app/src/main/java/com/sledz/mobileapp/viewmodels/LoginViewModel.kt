50efa32dcd15f35331f8f7bd5822188cd531d4fc
daronek6 <daro.pietrus@gmail.com>
2021-05-22 19:06:38 +0200

    -Dodano potrzebne biblioteki -Stworzono struktuę projektu dla architektury MVVM -Stworzono klienta API do komunikacji z serwerem -Stworzono interfejsy do API (do uzgodnienia śceiżki, metody i argumenty) -Stworzono klasy dla wykorzystywanych struktur danych -Stworzono podstawowe repozytorium danych (MainRepository)

---
+++ app/app/src/main/java/com/sledz/mobileapp/viewmodels/LoginViewModel.kt
@@ -0,0 +1,17 @@
+package com.sledz.mobileapp.viewmodels
+
+import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sledz.mobileapp.data.MainRepository
+import com.sledz.mobileapp.data.UserModel
+
+class LoginViewModel(app: Application): AndroidViewModel(app) {
+
+
+}
