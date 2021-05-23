647044c16044b81093583f189a584f246c0e9dcb
kondera <kgkondera@gmail.com>
2021-05-23 12:09:20 +0200

    Small step to di

--- app/app/src/main/java/com/sledz/mobileapp/repository/ProductRepository.kt
+++ app/app/src/main/java/com/sledz/mobileapp/repository/ProductRepository.kt
@@ -1,7 +1,13 @@
 package com.sledz.mobileapp.repository
 
-// tutaj będzie repozytorium z di product_api
-class ProductRepository {
+import com.sledz.mobileapp.data.remote.ProductApi
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class ProductRepository @Inject constructor(
+    private val api: ProductApi
+) {
 //    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
 //        val response = try {
 //            api.getPokemonList(limit, offset)
