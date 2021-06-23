46a43d88bce2f07dfc79e686ec1fc9dbb75ee67e
Radosław Kopiński <radoslaw@scythe-studio.com>
2021-06-23 11:37:07 +0200

    Made request return products already in database

--- backend/src/main/java/com/sledz/repositories/ProductRepository.java
+++ backend/src/main/java/com/sledz/repositories/ProductRepository.java
@@ -5,7 +5,10 @@ import com.sledz.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
     boolean existsByName(String name);
+    List<Product> findByNameContaining(String infix);
 }
